b140364e7d132d51f1f850b1c83517559e5e8d91
sergey.khashimov <sergey.khashimov@gns-it.com>
2021-12-16 13:28:03 +0200

    Add struct field list

--- generic.go
+++ generic.go
@@ -1,6 +1,10 @@
 package qbuilder
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
 
 type RowLevelLockMode int
 
@@ -42,4 +46,35 @@ func StrToDirection(str string) (SortDirection, error) {
 		return direction, nil
 	}
 	return SortDirectionDESC, fmt.Errorf("invalid direction %s", str)
+}
+
+func FieldList(obj interface{}, alias ...string) string {
+	objType := reflect.TypeOf(obj)
+	switch objType.Kind() {
+	case reflect.Ptr:
+		val := objType.Elem()
+		if val.Kind() != reflect.Struct {
+			argErr := fmt.Errorf("source must be a struct or struct pointer %s given", val.Kind().String())
+			panic(argErr)
+		}
+		objType = val
+	case reflect.Struct:
+	default:
+		argErr := fmt.Errorf("source must be a struct or struct pointer %s given", objType.Kind().String())
+		panic(argErr)
+	}
+	names := make([]string, 0)
+	for i := 0; i < objType.NumField(); i++ {
+		field := objType.Field(i)
+		if db, ok := field.Tag.Lookup("db"); ok {
+			if db != "" {
+				name := db
+				if len(alias) > 0 && alias[0] != "" {
+					name = fmt.Sprintf("%s.%s", alias[0], db)
+				}
+				names = append(names, name)
+			}
+		}
+	}
+	return strings.Join(names, ", ")
 }
